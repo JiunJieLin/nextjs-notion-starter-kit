873550de0e401a7a3d99d43d0700749054d0b5a7
JiunJieLin <144204804+JiunJieLin@users.noreply.github.com>
2023-12-12 12:15:54 +0800

    delete config

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import { IoMoonSharp } from '@react-icons/all-files/io5/IoMoonSharp'
 import { IoSunnyOutline } from '@react-icons/all-files/io5/IoSunnyOutline'
-import * as config from '@/lib/config'
 import { useDarkMode } from '@/lib/use-dark-mode'
 import styles from './styles.module.css'
 
